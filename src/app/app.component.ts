1950fb13cadc350fdc9609f5926b6bea90e032a8
Sean Perkins <sean@ionic.io>
2022-03-11 21:44:04 -0500

    chore(): reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NavController, Platform } from '@ionic/angular';
-import { Subscription } from 'rxjs';
-
-export class UserSettingsModel {
-
-  constructor(
-
-  ) { }
-
-}
 
 @Component({
   selector: 'app-root',
@@ -18,7 +7,7 @@ export class UserSettingsModel {
 })
 
 export class AppComponent {
-  public model: UserSettingsModel = new UserSettingsModel();
+  public model: any = {};
   public baseline: string | null = null;
   public baselineSet = true;
   public userForm: FormGroup;
@@ -40,11 +29,6 @@ export class AppComponent {
       ],
     });
 
-
-
-
   }
-
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
